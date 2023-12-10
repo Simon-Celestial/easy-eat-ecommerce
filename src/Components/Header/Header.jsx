d6e0f9438cd465b88391528b31a472dab4cc6ac1
Gasimli Ziya <gasimli.ziya@yandex.com>
2023-12-11 01:21:58 +0400

    header in progress

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import styles from "./Header.module.scss";
+import {MagnifyingGlass, ShoppingBag} from "@phosphor-icons/react";
+
 
 export const Header = () => {
     return (
         <header className={styles.siteHeader}>
             <section className={styles.headerContent}>
                 <div className={styles.headerLeft}>
-                    <div className={styles.HeaderBox}>
+                    <div className={styles.headerLogo}>
                         <img src="https://easyeat.ancorathemes.com/wp-content/uploads/2023/05/logo-inverse.png"
                              alt="Header Logo"/>
                     </div>
@@ -24,14 +26,54 @@ export const Header = () => {
                             <a>Shop</a>
                         </div>
 
-                        <a className={styles.navigationItems}>
+                        <div className={styles.navigationItems}>
                             <a href="#">Contacts</a>
 
-                        </a>
-
+                        </div>
                     </div>
                 </div>
                 <div className={styles.headerRight}>
+                    <div className={styles.headerRightItem}>
+                        <div className={styles.basketCount}>0</div>
+                        <ShoppingBag size={28}/>
+                    </div>
+                    <div className={styles.headerRightItem}>
+                        <MagnifyingGlass size={28}/>
+                    </div>
+                    <div className={styles.headerRightItem}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21">
+                            <g transform="translate(-2124 -2665)">
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2124 2665)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2132 2665)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2140 2665)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2124 2673)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2132 2673)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2140 2673)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2124 2681)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2132 2681)"></path>
+                                <path
+                                    d="M2.5,1.5a1,1,0,1,0,1,1,1,1,0,0,0-1-1M2.5,0A2.5,2.5,0,1,1,0,2.5,2.5,2.5,0,0,1,2.5,0Z"
+                                    transform="translate(2140 2681)"></path>
+                            </g>
+                        </svg>
+                    </div>
+
 
                 </div>
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from "./Main.module.scss";
+
 export const Main = () => {
     return (
-        <div className={styles.mainWrapper}>
-            Site Main
-        </div>
+        <main className={styles.mainWrapper}>
+            {/*ABOUT US SECTION*/}
+            <section className={styles.aboutUsSection}>
+                <div className={styles.aboutUsContent}>
+                    <div className={styles.aboutUsLeft}>
+                    </div>
+                    <div className={styles.aboutUsRight}>
+                        <div className={styles.spinningCircle}>
+                            <img src="https://easyeat.ancorathemes.com/wp-content/uploads/2023/05/circle-dotted-3.svg" alt=""/>
+                        </div>
+                        <div className={styles.floatingBurger}>
+                            <img
+                                src="https://easyeat.ancorathemes.com/wp-content/uploads/2023/05/parallax-img-5-copyright.png"
+                                alt=""/>
+
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </main>
     )
 }
