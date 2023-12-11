2bc17a8e7dba0617cb2d72481ae80667811d0c34
Gasimli Ziya <gasimli.ziya@yandex.com>
2023-12-11 16:14:38 +0400

    header menu dropdown in progress

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,35 +1,79 @@
 import styles from "./Header.module.scss";
 import {MagnifyingGlass, ShoppingBag} from "@phosphor-icons/react";
+import {NavMenuItems} from "./HeaderComponents/NavMenuItems.jsx";
 
 
 export const Header = () => {
+    const menuItems = [
+        {
+            id: 1,
+            drop: true,
+            label: "Home",
+            link: "/",
+            item1: "Burger Shop",
+            item2: "Steakhouse & Grill",
+            item3: "Pizzeria",
+            item4: "Food Truck",
+            item5: "Indian Restaurant",
+            item6: "Thai Food",
+            item7: "Coffee Bar",
+        },
+        {
+            id: 2,
+            drop: true,
+            toolsDrop: true,
+            label: "Pages",
+            link: "/",
+            item1: "About US",
+            item2: "FAQs",
+            item3: "Menu",
+            item4: "Our Services",
+            item5: "Our Team",
+            item6: "Pricing",
+            item7: "Tools",
+        },
+        {
+            id: 3,
+            drop: true,
+            singlePost: true,
+            label: "Blog ",
+            link: "/",
+            item1: "Standard",
+            item2: "List",
+            item3: "Portfolio",
+            item4: "Grid",
+            item5: "Single Post",
+        },
+        {
+            id: 4,
+            drop: true,
+            label: "Shop",
+            link: "/",
+            item1: "Product List",
+            item2: "Product Single",
+            item3: "Cart",
+            item4: "Checkout",
+            item5: "Wishlist Page",
+        },
+        {
+            id: 5,
+            drop: false,
+            label: "Contacts",
+            link: "/",
+        },
+    ];
     return (
         <header className={styles.siteHeader}>
             <section className={styles.headerContent}>
                 <div className={styles.headerLeft}>
-                    <div className={styles.headerLogo}>
+                    <a href="/" className={styles.headerLogo}>
                         <img src="https://easyeat.ancorathemes.com/wp-content/uploads/2023/05/logo-inverse.png"
                              alt="Header Logo"/>
-                    </div>
+                    </a>
                     <div className={styles.headerNavigation}>
-                        <div className={styles.navigationItems}>
-                            <a href="#">Home</a>
-                        </div>
-                        <div className={styles.navigationItems}>
-                            <a href="#">Pages</a>
-                        </div>
-                        <div className={styles.navigationItems}>
-                            <a href="#">Blog</a>
-
-                        </div>
-                        <div className={styles.navigationItems}>
-                            <a>Shop</a>
-                        </div>
-
-                        <div className={styles.navigationItems}>
-                            <a href="#">Contacts</a>
-
-                        </div>
+                        {menuItems.map((items) => (
+                            <NavMenuItems key={items.id} items={items}/>
+                        ))}
                     </div>
                 </div>
                 <div className={styles.headerRight}>
@@ -73,8 +117,6 @@ export const Header = () => {
                             </g>
                         </svg>
                     </div>
-
-
                 </div>
 
             </section>
